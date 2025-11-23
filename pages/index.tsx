dc76e3692e5400aa003b9d3a4046b2e83027f7bf
hamzadorbane33-sys <hamzadorbane33@gmail.com>
2025-11-23 08:33:07 +0100

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{background:'#000',color:'#fff',minHeight:'100vh',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',padding:24,textAlign:'center'}}>
-      <h1 style={{fontSize:'3rem',background:'linear-gradient(90deg,#c084fc,#ec4899)',WebkitBackgroundClip:'text',WebkitTextFillColor:'transparent'}}>Lockgram</h1>
-      <p style={{marginTop:16,fontSize:'1.25rem',color:'#ccc'}}>Own your chats, coins, future.</p>
-      <a href="https://twitter.com/LockgramApp" style={{marginTop:32,color:'#fff'}}>Twitter</a>
-    </main>
-  );
-}
